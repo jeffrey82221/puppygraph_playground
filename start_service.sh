9369a1ec6016165e69a9abc7554331a2336216f3
Jeffrey Lin <jeffrey82221@gmail.com>
2024-05-25 21:24:03 +0800

    Update start_service.sh

--- start_service.sh
+++ start_service.sh
@@ -1,8 +1,13 @@
+docker compose down
 docker compose up -d
 python create_demo_db.py
 docker cp demo.db duckdb:/home/share/demo.db
 echo 'uploading schema.json'
-curl -XPOST -H "content-type: application/json" \
+limit=30; start=5;
+until (( limit < (5-start) )); do 
+  echo 'do curl'
+  curl -XPOST -H "content-type: application/json" \
     --data-binary @./schema.json \
     --user "puppygraph:puppygraph123" \
-    localhost:8081/schema
+    localhost:8081/schema | grep -o "OK" && break
+done
