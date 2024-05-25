47c4eee228162c92c63237f180943013df41c203
jeffrey82221 <jeffrey82221@gmail.com>
2024-05-25 21:53:28 +0800

    feature: deploy pypi graph demo

---
+++ pypi_demo/README.md
@@ -0,0 +1,9 @@
+# Move 
+
+Move `schema.json` and `demo.db` of pypi graph here
+
+# Script 
+
+cp ../pypi_graph/schema.json .
+cp ../pypi_graph/duckdb/demo.db .
+
--- start_service.sh
+++ start_service.sh
@@ -1,13 +1,12 @@
 docker compose down
 docker compose up -d
 python create_demo_db.py
-docker cp demo.db duckdb:/home/share/demo.db
-echo 'uploading schema.json'
+docker cp pypi_demo/demo.db duckdb:/home/share/demo.db
 limit=30; start=5;
 until (( limit < (5-start) )); do 
   echo 'do curl'
   curl -XPOST -H "content-type: application/json" \
-    --data-binary @./schema.json \
+    --data-binary @./pypi_demo/schema.json \
     --user "puppygraph:puppygraph123" \
     localhost:8081/schema | grep -o "OK" && break
 done
